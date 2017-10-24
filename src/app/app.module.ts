8c1952c6ec5cea61cadbc721848c612b8b2a8c6e
francislagueu <lagson6@yahoo.fr>
2017-10-23 17:28:26 -0700

    Firebase set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+import {AngularFireAuthModule} from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { NavComponent } from './nav/nav.component';
     NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    
+    apiKey: 'AIzaSyDsZJ-cHLZTWPdciUhMmCIcEwjM4hWcI_8',
+    authDomain: 'eventset-1db67.firebaseapp.com',
+    databaseURL: 'https://eventset-1db67.firebaseio.com',
+    projectId: 'eventset-1db67',
+    storageBucket: 'eventset-1db67.appspot.com',
+    messagingSenderId: '1029896547310'
   }
 };
